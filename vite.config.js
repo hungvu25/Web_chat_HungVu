d4b25d5f4dc721ac91f327d2d40bc16a1f9908c8
vũ trường hùng <vhung9955@gmail.com>
2025-07-03 04:46:28 +0700

    s
    s

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001'
+export default {
+  version: 2,
+  builds: [
+    {
+      src: "index.html",
+      use: "@vercel/static-build",
+      "config": {
+        "distDir": "dist"
+      }
     }
-  }
-})
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/index.html"
+    }
+  ]
+}
